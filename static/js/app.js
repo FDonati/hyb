2a508e62a5f18aba162e336bbc3ea76419e722d7
FDonati <f.donati@cml.leidenuniv.nl>
2021-03-11 15:24:43 +0100

    Added first toggle pages and began table formatting

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,17 @@
 const scenInputBtn = document.querySelector('#ScenInputTables');
 
-scenInputBtn.addEventListener('click', function() {
-    alert('Hey!')
+const hybInputBtn = document.querySelector('#HybTables');
+
+const scenTabDiv = document.querySelector("#scenarios")
+const chartTabDiv = document.querySelector("#charts")
+const hybTabDiv = document.querySelector("#HybTables")
+
+scenInputBtn.addEventListener('click', function () {
+        scenTabDiv.style.display = "block";
+        chartTabDiv.style.display = "none";
+})
+
+hybInputBtn.addEventListener('click', function () {
+    scenTabDiv.style.display = "none";
+    chartTabDiv.style.display = "block";
 })
