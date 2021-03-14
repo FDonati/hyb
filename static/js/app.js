05b95935e44f56ed448f0c091bf330bb2c95bf07
FDonati <f.donati@cml.leidenuniv.nl>
2021-03-14 08:08:26 +0100

    better subdivied sections/partials (metrics), updated navbar and logos

--- static/js/app.js
+++ static/js/app.js
@@ -6,17 +6,16 @@ const scenTabDiv = document.querySelector("#scenarios")
 const chartTabDiv = document.querySelector("#charts")
 const hybTabDiv = document.querySelector("#HybTables")
 
-scenInputBtn.onclick = function() {
-    if (scenTabDiv.style.display !== 'none') {
-        scenTabDiv.style.display = 'none';
-    }
-    else {
-        scenTabDiv.style.display = 'block';
+scenInputBtn.onclick = function () {
+    if (scenTabDiv.style.display == "none") {
+        scenTabDiv.style.display = "block";
+        scenInputBtn.className = "is-active";
+    } else {
+        scenTabDiv.style.display = "none";
+        scenInputBtn.className = "";
     }
 };
 
 hybInputBtn.addEventListener('click', function () {
-    if (hybTabDiv.style.display == "none") {
-        hybTabDiv.style.display = "";
-    } 
+    hybInputBtn.className = "is-active";
 })
